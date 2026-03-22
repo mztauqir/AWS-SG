0c3a6eeb5881feabe12cbfe2035c2f529547fcf8
Zaid <38622990+mztauqir@users.noreply.github.com>
2026-03-22 18:52:49 -0500

    Update ingress and egress rules in security group

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "this" {
     from_port   = 5444
     to_port     = 5444
     protocol    = "tcp"
-    cidr_blocks = []
+    cidr_blocks = ["192.168.1.0/24"]
     self        = true
   }
 
